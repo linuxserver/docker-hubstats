e7c8b7491e9bbbb52c0f67c8ef7a5dd05f11371e
Paul Hendryx <phendryx@gmail.com>
2018-03-25 11:23:41 -0400

    update stats pull

--- app/stats.rb
+++ app/stats.rb
@@ -3,38 +3,77 @@ require "awesome_print"
 require "influxdb"
 require "yaml"
 
-config_file = File.dirname(__FILE__) + "/config/settings.yml"
-config = YAML.load_file(config_file)
+def processs_org(org)
+  arch = 'x86'
+  case(org)
+  when 'linuxserver' then
+    arch = 'x86'
+  when 'lsioarmhf' then
+    arch = 'armhf'
+  end
 
-email = config["email"]
-password = config["password"]
-org 	 = config["org"]
+  res = HTTParty.get(
+    "https://hub.docker.com/v2/repositories/#{org}/",
+    query: { page_size: 100 })
+  data = JSON.parse(res.body)
 
-data = { username: email,
-		 password: password }
+  pull_counts = {}
+  while data.include?('next') && data['next'].nil? == false
+    url = data['next']
+    res = HTTParty.get(url)
+    data = JSON.parse(res.body)
+    pull_counts.merge!(process_counts(data, arch))
+  end
 
-res = HTTParty.post("https://hub.docker.com/v2/users/login/", body: data).body
-token = JSON.parse(res)["token"]
-
-headers = { token: token}
-res = HTTParty.get(
-  "https://hub.docker.com/v2/repositories/#{org}/",
-  query: { page_size: 100 },
-  headers: headers)
-
-data = JSON.parse(res.body)
-
-pull_counts = {}
-data["results"].each do |repo|
-	pull_counts[repo["name"]] = repo["pull_count"]
+  pull_counts
 end
 
-influxdb = InfluxDB::Client.new database: "dockerhub_stats"
-pull_counts.each do |repo, count|
-	data = {
-	  values: { pull_count: count },
-	  tags:   { repo: repo }
-	}
+def process_counts(data, arch)
+  pull_counts = {}
+  data['results'].each do |repo|
+    name = repo['name']
+    count = repo['pull_count']
+    pull_counts[name] = { arch: arch, pull_count: count}
+  end
 
-	influxdb.write_point("dockerhub_stats", data)
+  pull_counts
 end
+
+def insert_counts(data)
+  influxdb = InfluxDB::Client.new database: "dockerhub_stats", port: 18086
+  data.each do |repo, repo_data|
+    influx_data = {
+      values: { pull_count: repo_data[:pull_count] },
+      tags:   { repo: repo, arch: repo_data[:arch] }
+    }
+    influxdb.write_point("dockerhub_stats", influx_data)
+  end
+
+end
+
+def run
+  orgs = nil
+
+  unless ENV['DOCKERHUB_ORGS'].nil?
+    orgs = ENV['DOCKERHUB_ORGS'].split(',')
+  end
+
+  if orgs.nil?
+    config_file = File.dirname(__FILE__) + "/config/settings.yml"
+    config = YAML.load_file(config_file)
+
+    orgs = config["orgs"]
+  end
+
+  ap orgs
+  exit
+
+  counts = {}
+  orgs.each do |org|
+    counts.merge!(processs_org(org))
+  end
+
+  insert_counts(counts)
+end
+
+run
